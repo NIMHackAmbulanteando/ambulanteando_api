67b8c3f30ab301ea5678c046e5e14e849ea2d4da
Gustavo Mora <gus@nubleer.com>
2016-07-23 19:41:58 -0500

    fix bugs on the enpoint tracks/

--- web/views/track_view.ex
+++ web/views/track_view.ex
@@ -1,11 +1,13 @@
 defmodule AmbulanteandoApi.TrackView do
 	use AmbulanteandoApi.Web, :view
+	require Logger
 
 	def render("create.json", %{track_view: track_view}) do
+		Logger.info "pase----"
 		%{data: render_one(track_view, AmbulanteandoApi.TrackView, "track.json")}
 	end
 
-	def render("track.json", %{track_view: track}) do
+	def render("track.json", %{track: track}) do
 		%{
 			total_kilometers: track.total_kilometers,
 			initial_spot_x: track.initial_spot_x,
